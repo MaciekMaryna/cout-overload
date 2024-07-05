05d3966628a22f20fbe232df664e8f7a9f81a735
MaciekMaryna <marynowski.maciej@gmail.com>
2024-07-05 15:32:41 +0200

    Scripts fine-tuning

---
+++ run.bat
@@ -0,0 +1,25 @@
+@echo off
+call clean.bat
+if %ERRORLEVEL% NEQ 0 (
+    echo Clean error: %ERRORLEVEL%
+    timeout /t 2
+)
+
+call build.bat
+if %ERRORLEVEL% NEQ 0 (
+    echo Build error: %ERRORLEVEL%
+    echo Check also build.log and error.log
+    exit
+) else (
+echo     _  _  _  _      _           _                     _
+echo   _^(_^)^(_^)^(_^)^(_^)_   ^(_^)       _ ^(_^)                   ^(_^)
+echo  ^(_^)          ^(_^)  ^(_^)    _ ^(_^)                      ^(_^)
+echo  ^(_^)          ^(_^)  ^(_^) _ ^(_^)                         ^(_^)
+echo  ^(_^)          ^(_^)  ^(_^)^(_^) _                          ^(_^)
+echo  ^(_^)          ^(_^)  ^(_^)   ^(_^) _
+echo  ^(_^)_  _  _  _^(_^)  ^(_^)      ^(_^) _                     _
+echo    ^(_^)^(_^)^(_^)^(_^)    ^(_^)         ^(_^)                   ^(_^)
+timeout /t 2
+main.exe
+)
+
